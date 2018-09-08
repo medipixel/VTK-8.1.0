2642233a47ed9d41a69c179a3bbbf91bfc5308be
Jay Lee <jay.lee@medipixel.io>
2018-09-08 13:50:57 +0900

    remove vtkDICOM in source, because cmake get vtkDICOM from git repository in build

--- source/Remote/vtkDICOM.remote.cmake
+++ source/Remote/vtkDICOM.remote.cmake
@@ -4,7 +4,7 @@
 
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
-  GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom release 0.8.7
-  GIT_TAG a29ca407608ad3b21ec5bb6f40a3aed0105f6e70
+  GIT_REPOSITORY https://github.com/medipixel/vtk-dicom.git
+  # vtk-dicom release 0.8.7, medipixel hotfix
+  GIT_TAG 4d2c92ffc48ae982d322365706701e0e5ba170a7
   )
